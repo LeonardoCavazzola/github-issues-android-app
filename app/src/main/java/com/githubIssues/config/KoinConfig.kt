9efac1e5c41714b1739c6ee52285307b12875ff5
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-10-31 13:01:56 -0300

    Add koin configuration

---
+++ app/src/main/java/com/githubIssues/config/KoinConfig.kt
@@ -0,0 +1,12 @@
+package com.githubIssues.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.githubIssues.gateway.GithubClient
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
+
+val appModule = module {
+    single<OkHttpClient> { OkHttpClient() }
+    single<GithubClient> { GithubClient() }
+    single<ObjectMapper> { buildObjectMapper() }
+}
