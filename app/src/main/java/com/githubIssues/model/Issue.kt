8d2e62ee0aa007050537ca67cb2f6fa967fe416e
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-11-01 23:33:32 -0300

    update models

--- app/src/main/java/com/githubIssues/model/Issue.kt
+++ app/src/main/java/com/githubIssues/model/Issue.kt
@@ -1,6 +1,17 @@
 package com.githubIssues.model
 
+import java.io.Serializable
+import java.time.ZonedDateTime
+
 class Issue(
     val title: String,
     val state: String,
-)
+    val user: User,
+    val body: String?,
+    val createdAt: ZonedDateTime
+) : Serializable {
+    companion object {
+        const val DATE_PATTERN_US = "yyyy-MM-dd'T'HH:mm:ssz"
+        const val DATE_PATTERN_BR = "dd-MM-yyyy HH:mm:ss z"
+    }
+}
---
+++ app/src/main/java/com/githubIssues/model/User.kt
@@ -0,0 +1,8 @@
+package com.githubIssues.model
+
+import java.io.Serializable
+
+class User(
+    val login: String,
+    val avatarUrl: String,
+): Serializable
