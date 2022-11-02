10ff9ca6e61286f1a12fa56627a773c34c8daa01
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-11-01 23:35:39 -0300

    add more beans to koin

--- app/src/main/java/com/githubIssues/config/KoinConfig.kt
+++ app/src/main/java/com/githubIssues/config/KoinConfig.kt
@@ -1,6 +1,7 @@
 package com.githubIssues.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.githubIssues.gateway.BitmapClient
 import com.githubIssues.gateway.GithubClient
 import okhttp3.OkHttpClient
 import org.koin.dsl.module
@@ -8,5 +9,6 @@ import org.koin.dsl.module
 val appModule = module {
     single<OkHttpClient> { OkHttpClient() }
     single<GithubClient> { GithubClient() }
+    single<BitmapClient> { BitmapClient() }
     single<ObjectMapper> { buildObjectMapper() }
 }
