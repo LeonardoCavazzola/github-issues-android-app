ca5fc11d2bbb8188331ff64c8aac4482ced98d96
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-11-01 23:34:55 -0300

    update jackson config

--- app/src/main/java/com/githubIssues/config/JacksonConfig.kt
+++ app/src/main/java/com/githubIssues/config/JacksonConfig.kt
@@ -2,10 +2,12 @@ package com.githubIssues.config
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 
 fun buildObjectMapper(): ObjectMapper {
     val objectMapper = ObjectMapper()
     objectMapper.findAndRegisterModules()
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    objectMapper.propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
     return objectMapper
 }
