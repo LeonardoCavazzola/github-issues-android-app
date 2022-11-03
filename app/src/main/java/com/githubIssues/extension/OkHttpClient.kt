15770a2ad3908fa227034ff033a6c13303f2344d
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-11-03 20:37:23 -0300

    add extension to turn the client reactive

--- app/src/main/java/com/githubIssues/extension/OkHttpClient.kt
+++ app/src/main/java/com/githubIssues/extension/OkHttpClient.kt
@@ -3,14 +3,12 @@ package com.githubIssues.extension
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
+import reactor.core.publisher.Mono
 import java.io.IOException
 
-fun Call.enqueue(
-    onFailure: (call: Call, e: IOException) -> Unit,
-    onResponse: (call: Call, response: Response) -> Unit
-) {
+fun Call.enqueue(): Mono<Response> = Mono.create { mono ->
     this.enqueue(object : Callback {
-        override fun onFailure(call: Call, e: IOException) = onFailure(call, e)
-        override fun onResponse(call: Call, response: Response) = onResponse(call, response)
+        override fun onFailure(call: Call, e: IOException) = mono.error(e)
+        override fun onResponse(call: Call, response: Response) = mono.success(response)
     })
 }
